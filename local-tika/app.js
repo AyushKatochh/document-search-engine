f303097fa38143db991971b270e966c0d7a60d8f
AyushKatochh <katochayush226@gmail.com>
2023-01-27 21:33:49 +0530

    v4

--- local-tika/app.js
+++ local-tika/app.js
@@ -5,13 +5,20 @@ const { Client } = require("elasticsearch");
 const elasticUrl = "http://localhost:9200";
 //const elasticUrl = "http://host.docker.internal:9200";
 //const elasticUrl = "http://elasticsearch:9200";
+const pdf = require('pdf-poppler');
 const multer = require('multer');
 const ejs = require('ejs')
 const esclient   = new Client({ node: elasticUrl });
 const elasticsearch = require('elasticsearch');
+const path = require("path")
+
 const mime = require('mime-types');
+
 const PdfExtractor = require('pdf-extractor').PdfExtractor;
 
+const directoryPath = path.join(__dirname, 'database');
+const fileType = mime.lookup(directoryPath)
+const directoryPathImage = path.join(__dirname, 'ayush'); 
 const app = express();
 
 const client = new elasticsearch.Client({
@@ -72,25 +79,49 @@ let intervalId;
 let results;
 app.post('/upload', upload.single('file'), (req, res) => {
   intervalId=setInterval(async () => {
-    try {
-      const filePath = 'database/' + req.file.originalname;
-      const fileType = mime.lookup(filePath)
-
-      if(fileType === 'application/pdf') {
-  let pdfExtractor = new PdfExtractor('./database', {
-   
-    pageRange: [1,10]
-});
- results = pdfExtractor.parse(filePath).then(function () {
-	console.log(results);
-  res.redirect("/")
-}).catch(function (err) {
-	console.error('Error: ' + err);
-});
-
-} else {
-
-       const response = await axios({
+     try {
+         const filePath = 'database/' + req.file.originalname;
+         const fileType = mime.lookup(filePath)
+        if(fileType === "application/pdf") {
+        const files = await fs.promises.readdir(directoryPath);
+        for (const file of files) {
+            if(path.extname(file) === '.pdf'){
+                let opts = {
+                    format: 'jpeg',
+                    out_dir: path.join(__dirname, 'ayush'),
+                    out_prefix: path.basename(file, path.extname(file)),
+                    page: null
+                }
+                await pdf.convert(path.join(directoryPath, file), opts);
+                console.log(`Successfully converted ${file}`);
+                const images = await fs.promises.readdir(directoryPathImage);
+                for (const image of images) {
+                    const filePath = path.join(directoryPathImage, image);
+                    const fileType = mime.lookup(filePath);
+                    const response = await axios({
+                        method: 'put',
+                        url: 'http://localhost:9998/tika',
+                        data: fs.createReadStream(filePath),
+                        headers: { 'Content-Type': fileType,
+                                    'Accept': 'text/plain',
+                                    "X-Tika-OCRLanguage" :"eng" }
+                    });
+                    console.log(response.data);
+                    const result = await client.index({
+                        index: 'data',
+                        id: image,
+                        type: 'text',  // use unique identifier
+                        body: {
+                            text: response.data
+                        },
+                        refresh: "true" //immediately available for search
+                    });
+                    console.log(result);
+                }
+            }
+        }
+        } else {
+            const response = await axios({
         method: 'put',
          url: 'http://localhost:9998/tika',
          //url: 'http://host.docker.internal:9998/tika',      
@@ -121,9 +152,10 @@ app.post('/upload', upload.single('file'), (req, res) => {
         console.log('Document already exists');
       }
 
-    } catch (error) {
-      console.error(error);
-    }
+    
+    } catch (err) {
+        console.log('Unable to scan directory: ' + err);
+    } 
   }, 60000);  // 120000 milliseconds = 2 minutes
 });
 
@@ -136,3 +168,8 @@ const PORT= 3004
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+
+
+
+
